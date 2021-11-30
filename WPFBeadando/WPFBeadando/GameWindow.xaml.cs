272bde8dfcd96b655bf818f5e475ccc6581ead17
O48WRX <kardyy81@gmail.com>
2021-11-30 07:39:20 +0100

    Game logic pt. 1

--- WPFBeadando/WPFBeadando/GameWindow.xaml.cs
+++ WPFBeadando/WPFBeadando/GameWindow.xaml.cs
@@ -19,7 +19,22 @@ namespace WPFBeadando
     /// </summary>
     public partial class GameWindow : Window
     {
+        public Random rnd = new Random();
         public HideTransfer hideIt;
+
+        public int[] deck = new int[] {2,3,4,5,6,7,8,9,10,10,10,10,
+                                       2,3,4,5,6,7,8,9,10,10,10,10,
+                                       2,3,4,5,6,7,8,9,10,10,10,10,
+                                       2,3,4,5,6,7,8,9,10,10,10,10};
+
+        public bool Doubling = false;
+        public int defaultTokens = 1000;
+        public int wonTokens;
+        public bool VDCanPull = true;
+        public int activeBet = 0;
+
+        public List<int> playerCards = new List<int>();
+        public List<int> dealerCards = new List<int>();
         public GameWindow(HideTransfer hide)
         {
             InitializeComponent();
@@ -30,6 +45,47 @@ namespace WPFBeadando
         {
             hideIt.Invoke();
             this.Close();
+        }
+
+        public int DrawRandomCard(int[] deck)
+        { 
+            return deck[rnd.Next(1, deck.Length + 1)];
+        }
+
+        public void StartGame()
+        {
+            GW_Bet.IsEnabled = true;
+
+            playerCards.Add(DrawRandomCard(deck));
+            playerCards.Add(DrawRandomCard(deck));
+
+            dealerCards.Add(DrawRandomCard(deck));
+            dealerCards.Add(DrawRandomCard(deck));
+
+            if (dealerCards[0] + dealerCards[1] == 17)
+            {
+                VDCanPull = false;
+            }
+            Doubling = false;
+        }
+
+        private void GW_Bet_Click(object sender, RoutedEventArgs e)
+        {
+            if (activeBet != 0)
+            {
+                MessageBox.Show("Már tett tétet!");
+                GW_Bet.IsEnabled = false;
+            }
+
+            int bet = 0;
+
+            if (!int.TryParse(GW_BetBox.Text, out bet))
+            {
+                MessageBox.Show("A megadott tét nem szám!");
+                return;
+            }
+
+            activeBet = bet;
         }
     }
 }
--- WPFBeadando/WPFBeadando/obj/Debug/net5.0-windows/Leaderboard.g.i.cs
+++ WPFBeadando/WPFBeadando/obj/Debug/net5.0-windows/Leaderboard.g.i.cs
@@ -69,7 +69,7 @@ namespace WPFBeadando {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/WPFBeadando;component/leaderboard.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/WPFBeadando;V1.0.0.0;component/leaderboard.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\Leaderboard.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
